7c147d66298b96d60e8415e79b0767e1bb616f86
yarzardriz-source <yarzardriz@gmail.com>
2026-06-12 09:22:02 +0630

    version name change

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.6.3"
+#define THISFIRMWARE "ArduCopter V4.6.3E-2+"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,6,3,FIRMWARE_VERSION_TYPE_OFFICIAL
